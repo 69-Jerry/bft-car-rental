a64a58d7869ce55c653c94d92c95bf087c53c059
Jerry-H01 <jerry@heavydealers.com>
2025-11-24 23:18:34 +0000

    site-map

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import ViteSitemapPlugin from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src")
-    }
-  }
+  plugins: [
+    react(),
+    ViteSitemapPlugin({
+      hostname: 'https://yourdomain.com', // your real domain
+      outDir: 'dist',
+    }),
+  ],
 })
